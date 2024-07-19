f91c3863836a0fb79486898ffdc74d89d710fbb3
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-07-19 20:19:38 +0800

    nvidia-smi
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- rdma-tools/image/install-tools.sh
+++ rdma-tools/image/install-tools.sh
@@ -40,24 +40,6 @@ ln -fs /usr/share/zoneinfo/UTC /etc/localtime
 
 apt-get install -y --no-install-recommends "${packages[@]}"
 
-
-# cuda
-apt install -y ca-certificates
-wget --no-check-certificate https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
-dpkg -i cuda-keyring_1.1-1_all.deb
-apt-get update
-
-# for nvidia-smi
-## apt-get -y install cuda-toolkit-12-5
-##apt-get install -y cuda-drivers-555
-#apt-get install -y nvidia-utils-555
-
-# https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/
-wget  --no-check-certificate \
-  https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/nvidia-utils-550_550.90.07-0ubuntu1_amd64.deb
-apt-get install -y ./*.deb
-
-
 apt-get purge --auto-remove
 apt-get clean
 rm -rf /var/lib/apt/lists/*
--- rdma-tools/image/test.sh
+++ rdma-tools/image/test.sh
@@ -27,7 +27,4 @@ which iperf3 &>/dev/null
 which ping &>/dev/null
 
 
-which nvidia-smi &>/dev/null
-
-
 exit 0
